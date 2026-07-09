80625b0ece36949c2d13b313e99859bf2728d83a
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2026-07-09 23:23:59 +0530

    next-config-update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "4a2e-2405-201-200a-184c-8414-9ba2-9b2b-b6d4.ngrok-free.app",
+  ],
 };
 
 export default nextConfig;
