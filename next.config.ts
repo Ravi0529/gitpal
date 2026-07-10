5c98ba0654191b7553f481cc6f7dc43b991f42b4
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2026-07-10 16:53:53 +0530

    github-app-integration-with-octokit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: [
-    "4a2e-2405-201-200a-184c-8414-9ba2-9b2b-b6d4.ngrok-free.app",
-  ],
+  allowedDevOrigins: ["supersmart-parthenia-prettily.ngrok-free.dev"],
 };
 
 export default nextConfig;
